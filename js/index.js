c6bf5f3d82516e01c0b1b1983eefecbf7fb9ac86
Van Minh Ngai <62783825+VanMinhNgai@users.noreply.github.com>
2022-01-07 13:48:55 -0500

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 let weather = {
-  apiKey: "API KEY goes here",
+  apiKey: process.env.NEW_API_KEY,
   fetchWeather: function (city) {
     fetch(
       'https://api.openweathermap.org/data/2.5/weather?q=' +
