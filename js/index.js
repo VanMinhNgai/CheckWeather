a4a5de720b4957a050051aa75a66f1ef3333656d
Van Minh Ngai <ngaiv@uwindsor.ca>
2022-02-02 12:37:30 -0500

    Securing APIKey

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 let weather = {
-  apiKey: "API goes here",
+  apiKey: key.Secret_API_KEY,
   fetchWeather: function (city) {
     fetch(
       'https://api.openweathermap.org/data/2.5/weather?q=' +
